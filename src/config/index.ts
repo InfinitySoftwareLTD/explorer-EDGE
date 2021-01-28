a0aa2392daa9c260134f1e30ba79dc48fc5ace9e
Infinity developers <developers@infinitysoftware.io>
2021-01-28 10:29:51 +0400

    Origin

---
+++ src/config/index.ts
@@ -0,0 +1,89 @@
+export const I18N = {
+  defaultLocale: "en-GB",
+  enabledLocales: ["en-GB", "en-US", "it-IT", "fr-FR", "nl-NL", "pl-PL", "pt-BR"],
+};
+
+export const brand = {
+  footer: {
+    url: "https://infinitysoftware.io",
+    text: "Infinity Software.",
+  },
+};
+
+export const knownWalletsUrls = {
+  mainnet: "https://raw.githubusercontent.com/ArkEcosystem/common/master/mainnet/known-wallets.json",
+  devnet: "https://raw.githubusercontent.com/ArkEcosystem/common/master/devnet/known-wallets.json",
+};
+
+export const socialMedia = [
+  {
+    name: "facebook",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 10 16",
+  },
+  {
+    name: "twitter",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "youtube",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 18 16",
+  },
+  {
+    name: "email",
+    url: "mailto:info@infinitysoftware.io",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "telegram",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "reddit",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "github",
+    url: "https://github.com/Plusid/core-explorer-infinity",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "bitcointalk",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "slack",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "discord",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "instagram",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "medium",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "soundcloud",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 20 16",
+  },
+  {
+    name: "minds",
+    url: "https://infinitysoftware.io",
+    viewBox: "0 0 14 16",
+  },
+];
---
+++ src/mixins/currency.ts
@@ -0,0 +1,66 @@
+import store from "@/store";
+import { BigNumber } from "@/utils";
+
+const locale = store.getters["ui/locale"];
+
+export default {
+  methods: {
+    // Note: due to BigNumber config the max decimals is 5
+    readableCrypto(value: string | undefined, appendCurrency = true, decimals = 5): string | void {
+      if (value) {
+        const bigNumberValue = BigNumber.make(value);
+        const normalizedValue: string = Number(bigNumberValue.dividedBy(1e5)).toLocaleString(locale, {
+          maximumFractionDigits: decimals,
+        });
+
+        return appendCurrency
+          ? `${normalizedValue} ${store.getters["network/symbol"] || store.getters["network/defaults"].symbol || ""}`
+          : normalizedValue;
+      }
+    },
+
+    readableCurrency(
+      value: string | number,
+      rate: number | null = null,
+      currency: string | null = null,
+      normalise = true,
+    ): string {
+      const currencyName: string = currency || store.getters["currency/name"];
+
+      let bigNumberValue = BigNumber.make(value);
+
+      if (normalise) {
+        bigNumberValue = bigNumberValue.dividedBy(1e5);
+      }
+
+      bigNumberValue = bigNumberValue.times(rate || BigNumber.make(store.getters["currency/rate"]));
+
+      const cryptos: { [key: string]: string } = {
+        ARK: "Ѧ",
+        BTC: "Ƀ",
+        ETH: "Ξ",
+        LTC: "Ł",
+      };
+
+      return [store.getters["network/token"], "BTC", "ETH", "LTC"].some((c) => currencyName.indexOf(c) > -1)
+        ? `${Number(bigNumberValue).toLocaleString(locale, {
+            maximumFractionDigits: 5,
+          })} ${cryptos[currencyName]}`
+        : Number(bigNumberValue).toLocaleString(locale, {
+            style: "currency",
+            currency: currencyName,
+          });
+    },
+
+    rawCurrency(value: number, currencyName: string): string {
+      return [store.getters["network/token"], "BTC", "ETH", "LTC"].some((c) => currencyName.indexOf(c) > -1)
+        ? value.toLocaleString(locale, {
+            maximumFractionDigits: 5,
+          })
+        : value.toLocaleString(locale, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          });
+    },
+  },
+};
