a6cd015d30e654f618af78dfad61c4674c9b138b
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2021-04-10 14:19:36 +0400

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,8 +11,8 @@ export const brand = {
 };
 
 export const knownWalletsUrls = {
-  // mainnet: "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/known-wallets/INFI/mainnet/known-wallets.json",
-  // devnet: "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/known-wallets/INFI/devnet/known-wallets.json",
+   mainnet: "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/known-wallets/INFI/mainnet/known-wallets.json",
+   devnet: "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/known-wallets/INFI/devnet/known-wallets.json",
 };
 
 export const socialMedia = [
